404a20abec0b102443cd4da9eca363ebbdcbf98d
Salim <salim.cse.engineer@gmail.com>
2024-12-29 17:20:36 +0530

    added more feature

--- api/index.js
+++ api/index.js
@@ -2,24 +2,24 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 const User = require("./models/User.js");
+const Place = require("./models/Place.js");
 const cookieParser = require("cookie-parser");
-const imageDownloader = require('image-downloader');
-const multer = require('multer');
-const fs = require('fs');
+const imageDownloader = require("image-downloader");
+const multer = require("multer");
+const fs = require("fs");
 // const path = require('path');
-require('dotenv').config();
+require("dotenv").config();
 const app = express();
 
 const bcryptSalt = bcrypt.genSaltSync(10);
-const jwtSecret = 'fasefraw4r5r3wq45wdfgw34twdfg';
-
+const jwtSecret = "fasefraw4r5r3wq45wdfgw34twdfg";
 
 // Middleware
 app.use(express.json());
 app.use(cookieParser());
-app.use('/uploads',express.static(__dirname+'/uploads'))
+app.use("/uploads", express.static(__dirname + "/uploads"));
 app.use(
   cors({
     credentials: true,
@@ -29,28 +29,26 @@ app.use(
 
 mongoose.connect(process.env.MONGO_URL);
 
-
 // Test Route
 app.get("/test", (req, res) => {
   res.json({ message: "Test Ok" });
 });
 
 // Register Route
-app.post('/register', async (req,res) => {
+app.post("/register", async (req, res) => {
   mongoose.connect(process.env.MONGO_URL);
-  const {name,email,password} = req.body;
+  const { name, email, password } = req.body;
 
   try {
     const userDoc = await User.create({
       name,
       email,
-      password:bcrypt.hashSync(password, bcryptSalt),
+      password: bcrypt.hashSync(password, bcryptSalt),
     });
     res.json(userDoc);
   } catch (e) {
     res.status(422).json(e);
   }
-
 });
 
 // Login Route
@@ -63,7 +61,7 @@ app.post('/login', async (req,res) => {
     if (passOk) {
       jwt.sign({
         email:userDoc.email,
-        id:userDoc._id,
+        id:userDoc._id
       }, jwtSecret, {}, (err,token) => {
         if (err) throw err;
         res.cookie('token', token).json(userDoc);
@@ -76,46 +74,121 @@ app.post('/login', async (req,res) => {
   }
 });
 
-app.get('/profile', (req,res) => {
+app.get("/profile", (req, res) => {
   mongoose.connect(process.env.MONGO_URL);
-  const {token} = req.cookies;
+  const { token } = req.cookies;
   if (token) {
     jwt.verify(token, jwtSecret, {}, async (err, userData) => {
       if (err) throw err;
-      const {name,email,_id} = await User.findById(userData.id);
-      res.json({name,email,_id});
+      const { name, email, _id } = await User.findById(userData.id);
+      res.json({ name, email, _id });
     });
   } else {
     res.json(null);
   }
 });
 
-app.post('/logout', (req,res) => {
-  res.cookie('token', '').json(true);
+app.post("/logout", (req, res) => {
+  res.cookie("token", "").json(true);
 });
 
-app.post('/upload-by-link', async (req,res) => {
-  const {link} = req.body;
-  const newName = 'photo' + Date.now() + '.jpg';
+app.post("/upload-by-link", async (req, res) => {
+  const { link } = req.body;
+  const newName = "photo" + Date.now() + ".jpg";
   await imageDownloader.image({
     url: link,
-    dest: __dirname + '/uploads/' +newName,
+    dest: __dirname + "/uploads/" + newName,
   });
   res.json(newName);
 });
 
-const photosMiddleware = multer({dest:'uploads'});
-app.post('/upload', photosMiddleware.array('photos', 100), async (req,res) => {
+const photosMiddleware = multer({ dest: "uploads" });
+app.post("/upload", photosMiddleware.array("photos", 100), async (req, res) => {
   const uploadedFiles = [];
   for (let i = 0; i < req.files.length; i++) {
-    const {path,originalname} = req.files[i];
-    const parts = originalname.split('.');
+    const { path, originalname } = req.files[i];
+    const parts = originalname.split(".");
     const ext = parts[parts.length - 1];
-    const newPath = path + '.' + ext;
+    const newPath = path + "." + ext;
     fs.renameSync(path, newPath);
-    uploadedFiles.push(newPath.replace('uploads/',''));
+    uploadedFiles.push(newPath.replace("uploads/", ""));
   }
   res.json(uploadedFiles);
+});
+
+app.post("/places", (req, res) => {
+  mongoose.connect(process.env.MONGO_URL);
+  const { token } = req.cookies;
+  const {
+    title,
+    address,
+    addedPhotos,
+    description,
+    price,
+    perks,
+    extraInfo,
+    checkIn,
+    checkOut,
+    maxGuests,
+  } = req.body;
+  jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+    if (err) throw err;
+    const placeDoc = await Place.create({
+      owner: userData.id,
+      price,
+      title,
+      address,
+      photos: addedPhotos,
+      description,
+      perks,
+      extraInfo,
+      checkIn,
+      checkOut,
+      maxGuests,
+    });
+    res.json(placeDoc);
+  });
+});
+
+app.get("/places", (req, res) => {
+  mongoose.connect(process.env.MONGO_URL);
+  const { token } = req.cookies;
+  jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+    const { id } = userData;
+    res.json(await Place.find({ owner: id }));
+  });
+});
+
+app.get('/places/:id', async (req,res) => {
+  mongoose.connect(process.env.MONGO_URL);
+  const {id} = req.params;
+  res.json(await Place.findById(id));
+});
+
+app.put('/places', async (req,res) => {
+  mongoose.connect(process.env.MONGO_URL);
+  const {token} = req.cookies;
+  const {
+    id, title,address,addedPhotos,description,
+    perks,extraInfo,checkIn,checkOut,maxGuests,price,
+  } = req.body;
+  jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+    if (err) throw err;
+    const placeDoc = await Place.findById(id);
+    if (userData.id === placeDoc.owner.toString()) {
+      placeDoc.set({
+        title,address,photos:addedPhotos,description,
+        perks,extraInfo,checkIn,checkOut,maxGuests,price,
+      });
+      await placeDoc.save();
+      res.json('ok');
+    }
+  });
+});
+
+app.get('/places', async (req,res) => {
+  mongoose.connect(process.env.MONGO_URL);
+  res.json( await Place.find() );
 });
 // Start Server
 app.listen(3000, () => {
--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -1,137 +1,63 @@
-import React, { act, useState } from "react";
-import { data, Link, useParams } from "react-router-dom";
-import Perks from "../Perks.jsx";
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import AccountNav from "../AccountNav";
 import axios from "axios";
-import PhotosUploader from "../PhotosUploader.jsx";
+
 function PlacesPage() {
   const { action } = useParams();
-  const [title, setTitle] = useState("");
-  const [address, setAddress] = useState("");
+  const [places, setPlaces] = useState([]);
 
-  const [description, setDescription] = useState("");
-  const [perks, setPerks] = useState([]);
-  const [extraInfo, setExtraInfo] = useState("");
-  const [checkIn, setCheckIn] = useState("");
-  const [checkOut, setCheckOut] = useState("");
-  const [maxGuests, setMaxGuests] = useState(1);
-  const [price, setPrice] = useState(100);
-  const [redirect, setRedirect] = useState(false);
-
-  function inputHeader(text) {
-    return <h2 className="text-2xl mt-4">{text}</h2>;
-  }
-  function inputDescription(text) {
-    return <p className="text-gray-500 text-sm">{text}</p>;
-  }
-  function preInput(header, description) {
-    return (
-      <>
-        {inputHeader(header)}
-        {inputDescription(description)}
-      </>
-    );
-  }
-
+  useEffect(() => {
+    axios.get("/places").then(({ data }) => {
+      setPlaces(data);
+    });
+  }, []);
 
   return (
     <div>
-      {action !== "new" && (
-        <div className="text-center">
-          <Link
-            className="inline-flex gap-1 bg-primary text-white py-2 px-6 rounded-full"
-            to={"/account/places/new"}
+      <AccountNav />
+      <div className="text-center">
+        <Link
+          className="inline-flex gap-1 bg-primary text-white py-2 px-6 rounded-full"
+          to={"/account/places/new"}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-6"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="size-6"
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M12 4.5v15m7.5-7.5h-15"
+            />
+          </svg>
+          Add new place
+        </Link>
+      </div>
+      <div className="mt-4">
+        {places.length > 0 &&
+          places.map((place) => (
+            <Link
+              key={place._id} // Adding the key prop here
+              to={"/account/places/" + place._id}
+              className="flex cursor-pointer gap-4 bg-gray-100 p-2 rounded-2xl"
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M12 4.5v15m7.5-7.5h-15"
-              />
-            </svg>
-            Add new place
-          </Link>
-        </div>
-      )}
-      {action === "new" && (
-        <div>
-          <form>
-            {preInput(
-              "Title",
-              "Title for your place. should be short and catchy as in advertisement"
-            )}
-            <input
-              type="text"
-              value={title}
-              onChange={(ev) => setTitle(ev.target.value)}
-              placeholder="tittle, for example: My lovely apartment"
-            />
-            {preInput("Address", "Address to this place")}
-
-            <input
-              type="text"
-              value={address}
-              onChange={(ev) => setAddress(ev.target.value)}
-              placeholder="adress"
-            />
-            {preInput("Photos", "more = better")}
-            <PhotosUploader/>
-            {preInput("Description", "description of the place")}
-            <textarea
-              value={description}
-              onChange={(ev) => setDescription(ev.target.value)}
-            />
-            {preInput("Perks", "select all the perks of your place")}
-            <div className="grid mt-2 gap-2 grid-cols-2 md:grid-cols-3 lg:grid-cols-6">
-              <Perks selected={perks} onChange={setPerks} />
-            </div>
-            {preInput("Extra info", "house rules, etc")}
-            <textarea
-              value={extraInfo}
-              onChange={(ev) => setExtraInfo(ev.target.value)}
-            />
-            {preInput(
-              "Check in&out times",
-              "add check in and out times, remember to have some time window for cleaning the room between guests"
-            )}
-            <div className="grid gap-2 sm:grid-cols-3">
-              <div>
-                <h3 className="mt-2 mb-1">Check in time</h3>
-                <input
-                  type="text"
-                  placeholder="14:00"
-                  value={checkIn}
-                  onChange={(ev) => setCheckIn(ev.target.value)}
-                />
+              <div className="w-32 h-32 bg-gray-300 grow shrink-0">
+                {place.photos.length > 0 && (
+                  <img src={place.photos[0]} alt="" />
+                )}
               </div>
-              <div>
-                <h3 className="mt-2 mb-1">Check out time</h3>
-                <input
-                  type="text"
-                  placeholder="11:00"
-                  value={checkOut}
-                  onChange={(ev) => setCheckOut(ev.target.value)}
-                />
+              <div className="grow-0 shrink">
+                <h2 className="text-xl font-semibold">{place.title}</h2>
+                <p className="text-sm mt-2">{place.description}</p>
               </div>
-              <div>
-                <h3 className="mt-2 mb-1">Max number of guest</h3>
-                <input
-                  type="number"
-                  value={maxGuests}
-                  onChange={(ev) => setMaxGuests(ev.target.value)}
-                />
-              </div>
-            </div>
-            <button className="primary my-4">Save</button>
-          </form>
-        </div>
-      )}
+            </Link>
+          ))}
+      </div>
     </div>
   );
 }
